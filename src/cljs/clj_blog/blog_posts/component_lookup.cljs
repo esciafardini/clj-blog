feb78c5e51d0ed3e91cfcc371c709c0c29faac48
esciafardini <edward.ciafardini@aclaimant.com>
2022-08-28 18:12:04 -0400

    Move component lookup to separate file for organizational reasons

---
+++ src/cljs/clj_blog/blog_posts/component_lookup.cljs
@@ -0,0 +1,6 @@
+(ns clj-blog.blog-posts.component-lookup
+  (:require
+   [clj-blog.blog-posts.blog-components-01 :as blog_components_01]))
+
+(def component-lookup
+  {"blog_components_01/first-entry" blog_components_01/first-entry})
