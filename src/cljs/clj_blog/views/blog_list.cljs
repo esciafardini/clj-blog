b8d86a62784795d91a3f2c8f3629b1b8091856ae
esciafardini <edward.ciafardini@aclaimant.com>
2022-09-02 19:36:37 -0400

    Add blog_list component to views

---
+++ src/cljs/clj_blog/views/blog_list.cljs
@@ -0,0 +1,18 @@
+(ns clj-blog.views.blog-list
+  (:require
+   [clj-blog.blog-posts.utils :refer [inst->date-str]]
+   [re-frame.core :as rf]))
+
+
+(defn blog-list []
+  (let [blog-posts (rf/subscribe [:blog-posts/list])]
+    (fn []
+      [:<>
+       [:h1 "Blog Posts"]
+       [:ul
+        (for [blog-post @blog-posts]
+          ^{:key (:title blog-post)}
+          [:li
+           [:a {:href (str "/#/blog-posts/" (:id blog-post))}
+           (str (inst->date-str (:date_created blog-post)) " - " (:title blog-post))]])]])))
+
