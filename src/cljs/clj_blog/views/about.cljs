1f26c992859904c8e3f901fe7ff01bb8f5d82dc1
esciafardini <edward.ciafardini@aclaimant.com>
2022-09-03 19:50:48 -0400

    Fix typo in About section

--- src/cljs/clj_blog/views/about.cljs
+++ src/cljs/clj_blog/views/about.cljs
@@ -3,8 +3,8 @@
 (defn about []
   [:div.column.is-three-fourths
    [:p "This Blogg was built with Clojure(Script) using the Luminus framework. The front-end is Re-Frame and Reagent, the back-end is a PostgreSQL instance.
-       This is my first fully realized & deployed full stack project.  It is meant to be a learning resources for myself and other people who are new to
-       Web Development and Functional Programming. I relied heavily on the book Web Development In Clojure (3rd Edition), YouTube (Kelvin Mai & On The Code Again), my co-workers,
+       This is my first fully realized & deployed full stack project.  It is meant to be a learning resource for myself and other people who are new to
+       Web Development and Functional Programming (like me). I relied heavily on the book Web Development In Clojure (3rd Edition), YouTube (Kelvin Mai & On The Code Again), my co-workers,
        and the Clojurians Slack.  Much gratitude to all."]
    [:ul
     [:li "Re-Frame"]
