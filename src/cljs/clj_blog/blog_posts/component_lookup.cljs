879e571ad14c6aafae9759ef3eeafdd021b3e17a
esciafardini <edward.ciafardini@aclaimant.com>
2022-12-14 11:52:34 -0500

    Adding a blog post

--- src/cljs/clj_blog/blog_posts/component_lookup.cljs
+++ src/cljs/clj_blog/blog_posts/component_lookup.cljs
@@ -1,11 +1,14 @@
 (ns clj-blog.blog-posts.component-lookup
   (:require
    [clj-blog.blog-posts.blog-components-01 :as blog_components_01]
-   [clj-blog.blog-posts.blog-components-02 :as blog_components_02]))
+   [clj-blog.blog-posts.blog-components-02 :as blog_components_02]
+   [clj-blog.blog-posts.blog-components-03 :as blog_components_03]
+   ))
 
 (def component-lookup
   {"blog_components_01/first-entry" blog_components_01/first-entry
    "blog_components_01/higher-order-functions" blog_components_01/higher-order-functions
    "blog_components_01/css-quest" blog_components_01/css-quest
    "blog_components_02/idioms" blog_components_02/idioms
-   "blog_components_02/little-schemer-chapter8" blog_components_02/little-schemer-chapter8})
+   "blog_components_02/little-schemer-chapter8" blog_components_02/little-schemer-chapter8
+   "blog_components_03/art-of-sql" blog_components_03/art-of-sql})
--- src/cljs/clj_blog/views/test.cljs
+++ src/cljs/clj_blog/views/test.cljs
@@ -2,9 +2,10 @@
   (:require
    [clj-blog.blog-posts.blog-components-01 :refer [css-quest]]
    [clj-blog.blog-posts.blog-components-02 :refer [little-schemer-chapter8 idioms]]
+   [clj-blog.blog-posts.blog-components-03 :refer [art-of-sql]]
    ))
 
 (defn test-view []
   [:div.column.is-two-thirds
    ;put yr draft blog component here
-   [little-schemer-chapter8]])
+   [art-of-sql]])
